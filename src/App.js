e0e26f3270e68f4612765d21c2570b13fc938b72
Akshayak8 <srigadaakshay@gmail.com>
2024-12-30 15:21:33 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-// App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/Landingpage";
 import Careers from "./components/Careers";
 import Footer from "./components/Footer";
-import "./App.css";
 import Application from "./components/Application";
+import ErrorPage from "./components/Errorpage"; // Import the ErrorPage component
+import "./App.css";
 
 function App() {
   return (
@@ -15,6 +15,8 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/careers" element={<Careers />} />
           <Route path="/Application" element={<Application />} />
+          {/* Catch-all Route for handling undefined paths */}
+          <Route path="*" element={<ErrorPage errorType="notFound" />} />
         </Routes>
       </div>
       <Footer />
